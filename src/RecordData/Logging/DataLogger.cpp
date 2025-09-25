ded2d27c88ee2af66393e7be8652474bfcd29ce1
Drew Brandt <ahbrandt@umd.edu>
2025-09-25 13:47:56 -0400

    Initial Commit

--- src/RecordData/Logging/DataLogger.cpp
+++
@@ -1,6 +0,0 @@
-#include "DataLogger.h"
-
-namespace astra
-{
-    DataLogger DataLogger::_global{nullptr, 0, nullptr, 0};
-}
--- src/RecordData/Logging/EventLogger.cpp
+++
@@ -1,6 +0,0 @@
-#include "EventLogger.h"
-
-namespace astra
-{
-    EventLogger EventLogger::_global{nullptr, 0};
-}
